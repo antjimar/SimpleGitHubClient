059ba8bcd4d1dfdb7629eff0ef464c612328a700
ANTONIO JIMÉNEZ MARTÍNEZ <antonio.jimenez2@gmail.com>
2015-11-27 16:56:38 +0100

    Create coreData stack file and grouped in folders (core data stack and xctdatamodel)

---
+++ Test_iOS_antjimar/Test_iOS_antjimar/Model/CoreDataStack/CoreDataStack.h
@@ -0,0 +1,13 @@
+//
+//  CoreDataStack.h
+//  Test_iOS_antjimar
+//
+//  Created by ANTONIO JIMÉNEZ MARTÍNEZ on 27/11/15.
+//  Copyright © 2015 Antonio. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CoreDataStack : NSObject
+
+@end
---
+++ Test_iOS_antjimar/Test_iOS_antjimar/Model/CoreDataStack/CoreDataStack.m
@@ -0,0 +1,13 @@
+//
+//  CoreDataStack.m
+//  Test_iOS_antjimar
+//
+//  Created by ANTONIO JIMÉNEZ MARTÍNEZ on 27/11/15.
+//  Copyright © 2015 Antonio. All rights reserved.
+//
+
+#import "CoreDataStack.h"
+
+@implementation CoreDataStack
+
+@end
