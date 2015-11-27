f3fed09ce30b7734d7f360ce3efe73d983cb9ea5
ANTONIO JIMÉNEZ MARTÍNEZ <antonio.jimenez2@gmail.com>
2015-11-27 16:25:31 +0100

    Initial commit and gitignore

---
+++ Test_iOS_antjimar/Test_iOS_antjimar/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Test_iOS_antjimar
+//
+//  Created by ANTONIO JIMÉNEZ MARTÍNEZ on 27/11/15.
+//  Copyright © 2015 Antonio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ Test_iOS_antjimar/Test_iOS_antjimarTests/Test_iOS_antjimarTests.m
@@ -0,0 +1,39 @@
+//
+//  Test_iOS_antjimarTests.m
+//  Test_iOS_antjimarTests
+//
+//  Created by ANTONIO JIMÉNEZ MARTÍNEZ on 27/11/15.
+//  Copyright © 2015 Antonio. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Test_iOS_antjimarTests : XCTestCase
+
+@end
+
+@implementation Test_iOS_antjimarTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
