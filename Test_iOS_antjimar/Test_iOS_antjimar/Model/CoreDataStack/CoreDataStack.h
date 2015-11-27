6d4cf892205f2faa60fc7781100b4649c70b874b
ANTONIO JIMÉNEZ MARTÍNEZ <antonio.jimenez2@gmail.com>
2015-11-27 17:04:24 +0100

    CoreDataStack with 3 context (2 in the background an 1 in the mainThread). 2 public context (1 in the background to write data into a disk, and 1 in the main thread to read data fro the disk)

--- Test_iOS_antjimar/Test_iOS_antjimar/Model/CoreDataStack/CoreDataStack.h
+++ Test_iOS_antjimar/Test_iOS_antjimar/Model/CoreDataStack/CoreDataStack.h
@@ -6,8 +6,21 @@
 //  Copyright © 2015 Antonio. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+#import <UIKit/UIKit.h>
+
+UIKIT_EXTERN NSString * const managedObjectContextDidMergeChangesNotification;
+
+typedef void(^CoreDataManagerCompletionBlock)(BOOL finished);
 
 @interface CoreDataStack : NSObject
+
+@property (nonatomic, readonly, strong) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, readonly, strong) NSManagedObjectContext *writeManagedObjectContext;
+
++ (instancetype)sharedInstance;
+- (void)cleanAllLocalStorage;
+- (void)deleteAllObjectsForEntity:(NSEntityDescription *)entityDescription completion:(CoreDataManagerCompletionBlock)completion;
+- (void)deleteAllObjectsForEntity:(NSEntityDescription *)entityDescription;
 
 @end
